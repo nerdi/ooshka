1fe30025b3a4158e6816a5189e2469a07dec26e8
Dan Solo <dan@nerdi.com>
2014-02-21 08:18:55 +1100

    Add rspec and capybara

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,3 +10,20 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 Ooshka::Application.config.secret_key_base = 'e5247156ebe9e98a35ed6536eccb305af11c055f0fdbb5de81eaa9a64b09ce6593452b1d8c11de5f7050564ec8d20cda3057fb7e282734edbbc1866f36fdfc11'
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Ooshka::Application.config.secret_key_base = secure_token
